89f69f01b045dcac54e06d1b876e47f4eb68a6c2
walidrab27 <walidrab27@gmail.com>
2026-06-04 18:51:07 +0300

    Fix some errors in deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/Portfolio-Island/",
   plugins: [react()],
+  base: '/Portfolio-Island/'
 })
